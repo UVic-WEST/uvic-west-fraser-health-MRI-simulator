8d09a0acee4884683a53fd75b00a774b96ce40da
natalyaa-01 <natalyaa01@icloud.com>
2026-03-01 15:35:50 -0800

    Refactor LightConfig attributes and validation

--- backend/light_config.py
+++ backend/light_config.py
@@ -6,19 +6,21 @@ from typing import Optional
 @dataclass
 class LightConfig:
   """light settings for a single cycle"""
-  sound_name: str
-  sound_intensity: float   #0.0-1.0 (to adjust percentage of full brightness)
-  sound_frequency: Optional[float] = None   #in Hz; used if lights are pulsing
+  light_name: str
+  intensity: float   #0.0-1.0 (to adjust percentage of full brightness)
+  frequency: Optional[float] = None   #in Hz; used if lights are pulsing
 
   def __post_init__(self):
     """
     verify that the light data is formatted correctly and the values are reasonable
     """
-    if 0.0 <= self.sound_intensity <= 1.0:
+    if 0.0 <= self.intensity <= 1.0:
       raise ValueError("instensity is out of range: must be between 0.0 and 1.0 (inclusive)")
-    if self.sound_frequency is not None and self.frequency <= 0:
+    if self.frequency is not None and self.frequency <= 0:
       raise ValueError("frequency must be positive")
 
   def __repr__(self):
     """return string representation of light data"""
-    
+    return (f"LightConfig info for {self.light_name}: \n",
+            f"\tlight intensity: {self.intensity}\n",
+            f"\tlight frequency: {self.frequency}\n)
