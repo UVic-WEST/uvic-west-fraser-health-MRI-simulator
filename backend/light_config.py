aba3783f4f2fbbfc654f5f2fe67dac24205ce16b
natalyaa-01 <natalyaa01@icloud.com>
2026-03-01 17:48:57 -0800

    Fix docstring formatting in light_config.py

--- backend/light_config.py
+++ backend/light_config.py
@@ -11,9 +11,7 @@ class LightConfig:
   frequency: Optional[float] = None   #in Hz; used if lights are pulsing
 
   def __post_init__(self):
-    """
-    verify that the light data is formatted correctly and the values are reasonable
-    """
+    """verify that the light data is formatted correctly and the values are reasonable"""
     if 0.0 <= self.intensity <= 1.0:
       raise ValueError("instensity is out of range: must be between 0.0 and 1.0 (inclusive)")
     if self.frequency is not None and self.frequency <= 0:
