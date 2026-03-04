4c406545f5e7f5c4c20cdab4828042bbdb6ef925
felicitymoro <felicitymoro@uvic.ca>
2026-03-04 01:47:04 -0800

    basic timer functionality made, need to connect with cycle running page and find font for timer touches on issues #45

--- frontend/TimerWidget.py
+++ frontend/TimerWidget.py
@@ -1,12 +1,8 @@
-import sys
 from PySide6.QtWidgets import (
     QGridLayout,
-    QMainWindow,
-    QStackedLayout,
-    QVBoxLayout,
     QWidget,
     QLabel,
-    QApplication
+    QPushButton
 )
 from PySide6.QtGui import(
     QPixmap
@@ -22,8 +18,7 @@ class TimerWidget(QWidget):
         super().__init__()
         self.setFixedSize(403,149)
         self.main_layout = QGridLayout()
-        self.total_time_ms = None
-        self.cur_time_ms = None
+        self.rem_time_s = None
 
         timer_box_asset_path = 'timer_box.png'
         timer_box_asset_pix = QPixmap(timer_box_asset_path)
@@ -31,22 +26,45 @@ class TimerWidget(QWidget):
         self.timer_box.setPixmap(timer_box_asset_pix)
         self.timer_box.setScaledContents(True)
         self.timer_box.setAlignment(Qt.AlignCenter)
-        self.main_layout.addWidget(self.timer_box)
+        self.main_layout.addWidget(self.timer_box,0,0)
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update_countdown)
-        self.main_layout.addWidget(self.timer)
+        self.timer_label = QLabel("00:00")
+        self.timer_label.setStyleSheet(
+            "color: black; \n font-size: 36px;") 
+        self.timer_label.setAlignment(Qt.AlignCenter)
+        self.main_layout.addWidget(self.timer_label,0,0)
+
+        self.dumbutton = QPushButton("start")
+        self.dumbutton.clicked.connect(self.start_countdown)
+        self.main_layout.addWidget(self.dumbutton)
 
         self.setLayout(self.main_layout)
 
-    def start_countdown(self, time_ms:int):
-        self.timer.start(time_ms)
+    def get_time(self, time_ms) -> str:
+        minutes = time_ms // 60
+        seconds = time_ms - (minutes*60)
+        return f"{minutes:02}:{seconds:02}"
+
+    def start_countdown(self):
+        time = 10
+        self.rem_time_s = time
+        self.timer_label.setText(self.get_time(self.rem_time_s))
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_countdown)
+
+        self.timer.start(1000) 
+        #1000 gives it an offset of 1s before starting.
 
     def update_countdown(self):
-        if self.cur_time_ms > 0:
-            self.cur_time_ms -= 1
-            # Update GUI label with new time
+        if self.rem_time_s > 0:
+            self.rem_time_s -= 1
+            self.timer_label.setText(self.get_time(self.rem_time_s))
         else:
             self.timer.stop()
+            self.cycle_completed()
+
+    def cycle_completed(self):
+        pass
+        #emit that cycle is done
 
 
