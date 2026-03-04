0a51e850d2fa036aca86e769fc72bb37f830a91b
felicitymoro <felicitymoro@uvic.ca>
2026-03-04 10:19:51 -0800

    This is the general implementation for the timer box and formatted to properly run with the project. It is not connected to the running cycle page yet and requires UI updates, but has functions to start from cycle running page when necessary. touches issue #60 closes no issues

--- frontend/running_cycle_widgets/TimerWidget.py
+++ frontend/running_cycle_widgets/TimerWidget.py
@@ -14,13 +14,18 @@ from PySide6.QtCore import (
         
 class TimerWidget(QWidget):
     def __init__(self):
+        '''
+        Timer widget for the cycle running page.
+        Call .start_countdown(time_s)
+        '''
         #write text that gets changed, Qtimer is hardware
         super().__init__()
         self.setFixedSize(403,149)
         self.main_layout = QGridLayout()
         self.rem_time_s = None
 
-        timer_box_asset_path = 'timer_box.png'
+        #create the bg of the widget
+        timer_box_asset_path = 'resources/running_page_assets/timer_box.png'
         timer_box_asset_pix = QPixmap(timer_box_asset_path)
         self.timer_box = QLabel()
         self.timer_box.setPixmap(timer_box_asset_pix)
@@ -28,26 +33,39 @@ class TimerWidget(QWidget):
         self.timer_box.setAlignment(Qt.AlignCenter)
         self.main_layout.addWidget(self.timer_box,0,0)
 
+        #create label that actually displays the time on the widget. this gets updated by update_countdown
         self.timer_label = QLabel("00:00")
         self.timer_label.setStyleSheet(
             "color: black; \n font-size: 36px;") 
         self.timer_label.setAlignment(Qt.AlignCenter)
         self.main_layout.addWidget(self.timer_label,0,0)
 
+        #REMOVE!!!! dummy button to remove after running cycle page is comepleted
         self.dumbutton = QPushButton("start")
         self.dumbutton.clicked.connect(self.start_countdown)
         self.main_layout.addWidget(self.dumbutton)
 
         self.setLayout(self.main_layout)
 
-    def get_time(self, time_ms) -> str:
-        minutes = time_ms // 60
-        seconds = time_ms - (minutes*60)
+    def start_countdown (self, time_s):
+        '''
+        call this to set and start the timer (in seconds)
+        '''
+        self.rem_time_s = time_s
+        self.start()
+    
+    def get_time(self, time_s) -> str:
+        '''
+        returns the time as a string for countdown label
+        '''
+        minutes = time_s // 60
+        seconds = time_s - (minutes*60)
         return f"{minutes:02}:{seconds:02}"
 
-    def start_countdown(self):
-        time = 10
-        self.rem_time_s = time
+    def start(self):
+        '''
+        starts actually running the timer based of the value of rem_time_s
+        '''
         self.timer_label.setText(self.get_time(self.rem_time_s))
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_countdown)
@@ -56,6 +74,9 @@ class TimerWidget(QWidget):
         #1000 gives it an offset of 1s before starting.
 
     def update_countdown(self):
+        '''
+        this function updates the timer label to display the remaining time left on the cycle on screen
+        '''
         if self.rem_time_s > 0:
             self.rem_time_s -= 1
             self.timer_label.setText(self.get_time(self.rem_time_s))
