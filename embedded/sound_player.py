e1c731040ca69edaf64c9c9e185a427d8e0a0db4
megan <megand0905@gmail.com>
2026-03-07 20:06:14 -0800

    Add SoundConfig and SoundPlayer with layer 2 stubs

--- embedded/sound_player.py
+++ embedded/sound_player.py
@@ -1,39 +1,93 @@
-from sound import Sound
+import subprocess 
+from embedded.sound_config import SoundConfig
 
 class SoundPlayer:
     """
-    Sound player 
+    Handles sound playback requests.
 
-    Communicates directly to the raspberry pi's output audio 
+    This class will eventually communicate with the Raspberry Pi OS
+    audio system (Layer 2). For now, those calls are stubbed.
     """
+
     def __init__(self):
-        self.current_sound = None
+        self.current_sound = None # Currently playing sound 
+        self.current_volume = 0 # Session-specific sound volume 
 
-    def play(self, sound: Sound):
-        # start sound playing process
+    def play(self, sound: SoundConfig) -> str:
+        """
+            Play a sound using the configuration provided.
+
+            Stub for Layer 2 communication.
+            **Untested** hardware implementation
+        """
         self.current_sound = sound
-        formatted_volume = sound.volume * 100
-        return ("Playing sound %s at %d") % (self.current_sound.path, formatted_volume)
+        self.current_volume = sound.volume
 
-    def stop(self):
-        # kill sound playing process
+        try:
+            subprocess.run(["amixer", "sset", "PCM,0", f"{self.current_volume}%"], check=True)
+            subprocess.run(["aplay", self.current_sound.file_name], check=True)
+
+        except subprocess.CalledProcessError as e:
+            return "Failed to play sound, error: {e}"
+
+        return f"Playing sound {self.current_sound.file_name} at {self.current_volume}%"
+
+    def stop(self) -> str:
+        """
+            Stops all sound playing (aplay) processes.
+        
+            Stub for Layer 2 communication.
+            **Untested** hardware implementation
+        """
         self.current_sound = None
+        
+        try:
+            subprocess.run(["killall", "aplay"], check=True)
+
+        except subprocess.CalledProcessError as e:
+            return "Failed to stop sound, error: {e}"
+            
         return "Stopped current sound"
 
 
-    def incr_volume(self):
-        # increment volume
+    def incr_volume(self) -> str:
+        """
+            Increment volume of currently playing sound by 10%
+        
+            Stub for Layer 2 communication.
+            **Untested** hardware implementation
+        """
         if not self.current_sound:
             return "No sound is currently playing"
         
-        self.current_sound.volume = max(1.0, self.current_sound.volume + 0.1)
-        return f"Set volume to {self.current_sound.volume}"
+        self.current_volume = min(100, self.current_volume + 10)
+
+        try:
+            subprocess.run(["amixer", "sset", "PCM,0", f"{self.current_volume}%"], check=True)
+            
+        except subprocess.CalledProcessError as e:
+            return "Failed to increment sound, error: {e}"
+        
+        return f"Set volume to {self.current_volume}%"
 
 
-    def decr_volume(self):
-        # decrement volume 
+    def decr_volume(self) -> str:
+        """
+            Decrement volume of currently playing sound by 10%
+        
+            Stub for Layer 2 communication.
+            **Untested** hardware implementation
+        """
         if not self.current_sound:
             return "No sound is currently playing"
         
-        self.current_sound.volume = min(0, self.current_sound.volume - 0.1)
-        return f"Set volume to {self.current_sound.volume}"
+        self.current_volume = max(0, self.current_volume - 10)
+        
+        try:
+            subprocess.run(["amixer", "sset", "PCM,0", f"{self.current_volume}%"], check=True)
+            
+        except subprocess.CalledProcessError as e:
+            return "Failed to decrement sound, error: {e}"
+        
+
+        return f"Set volume to {self.current_volume}%"
