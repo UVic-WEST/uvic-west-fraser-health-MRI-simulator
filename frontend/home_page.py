c6057aedfe07d6610089ac86c936a123134b9328
EmilyNMurray <emilymurray@uvic.ca>
2026-03-08 16:19:36 -0700

    Updated the Homepage with a cycle selection feature

--- frontend/home_page.py
+++ frontend/home_page.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import(
     QWidget,
     QGridLayout
 )
+from PySide6.QtCore import Qt
 
 from frontend.home_page_widgets.cycle_player_widget import CyclePlayerWidget
 
@@ -13,13 +14,23 @@ class HomePage(QWidget):
         self.parent = parent
         self.controller = controller
         self.main_layout = QGridLayout()
+        self.main_layout.setRowMinimumHeight(0, 90)
+        self.cur_cycle = None
 
         #setting up and organizing widgets
         self.play_widget = CyclePlayerWidget(self)
-        self.main_layout.addWidget(self.play_widget)
+        self.main_layout.addWidget(self.play_widget, 1, 0, Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
 
         self.setLayout(self.main_layout)
+
+    def set_cur_cycle(self, cycle_name):
+        self.cur_cycle = cycle_name
+
+    # Backward-compatible alias while other callers are being updated.
+    def set_selected_cycle(self, cycle_name):
+        self.set_cur_cycle(cycle_name)
         
     def play_selected_cycle(self):
-        self.parent.play_cycle()
+        print("Current cycle confirmed to play:", self.cur_cycle)
+        self.parent.play_cycle(self.cur_cycle)
     
--- frontend/home_page_widgets/play_square_widget.py
+++ frontend/home_page_widgets/play_square_widget.py
@@ -6,6 +6,8 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtGui import (
     QPixmap,
+    QIcon,
+    QFont,
 )
 from PySide6.QtCore import (
     Qt, 
@@ -22,22 +24,32 @@ class PlaySquareWidget(QWidget):
         super().__init__(parent)
         self.parent = parent
         main_layout = QGridLayout()
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(0)
         self.setFixedSize(353,273)
 
         ## this has the current cycle information, gets updated from CycleplayerWidget
         self.cur_cycle = None
 
         #asset paths
-        blue_box_asset_path = 'resources/home_page_assets/blue_cycle_player.png'
         play_button_asset_path = 'resources/home_page_assets/cycle_play_button.png'
+        clock_icon_asset_path = 'resources/frontend_common_assets/whiteclockicon.png'
         
         #setting up widgets
         #for the blue_box that has the play button in it
-        blue_box_pix = QPixmap(blue_box_asset_path)
-        blue_box = QLabel()
-        blue_box.setScaledContents(True)
-        blue_box.setPixmap(blue_box_pix)
-        blue_box.setAlignment(Qt.AlignCenter)
+        blue_box = QWidget()
+        blue_box.setAttribute(Qt.WA_StyledBackground, True)
+        blue_box.setStyleSheet("""
+            QWidget {
+                background-color: #0474BA;
+                border-radius: 12px;
+            }
+        """)
+
+        # Keep icon/button on top of the blue box so transparent pixels show blue, not page white.
+        blue_box_layout = QGridLayout(blue_box)
+        blue_box_layout.setContentsMargins(12, 6, 12, 12)
+        blue_box_layout.setSpacing(0)
         
         #for the actual play button to press the cycle
         cycle_play_button_pix = QPixmap(play_button_asset_path)
@@ -45,13 +57,29 @@ class PlaySquareWidget(QWidget):
         cycle_play_button_size = cycle_play_button_pix.width()
 
         cycle_play_button.setFixedSize(cycle_play_button_size,cycle_play_button_size)
-        cycle_play_button.setIcon(cycle_play_button_pix)
+        cycle_play_button.setIcon(QIcon(cycle_play_button_pix))
         cycle_play_button.setIconSize(QSize(cycle_play_button_size,cycle_play_button_size))
         make_button_circle(cycle_play_button,cycle_play_button_size)
 
+        #for the clock icon
+        clock_icon_pix = QPixmap(clock_icon_asset_path)
+        clock_icon = QLabel()
+        clock_icon.setStyleSheet("background: transparent;")
+        clock_icon.setPixmap(clock_icon_pix)
+        clock_icon.setScaledContents(True)
+        clock_icon.setFixedSize(40, 40)
+
+        #for the cycle title at the top of the play square
+        cycle_title = QLabel("Full Cycle")
+        cycle_title.setFont(QFont("Ubuntu", 30))
+        cycle_title.setStyleSheet("color: white; background: transparent;")
+        cycle_title.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+
         #organize widget layout
-        main_layout.addWidget(blue_box)
-        main_layout.addWidget(cycle_play_button, 0, 0, 1, 1, Qt.AlignmentFlag.AlignCenter)
+        blue_box_layout.addWidget(cycle_title, 0, 0, 1, 1, Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+        blue_box_layout.addWidget(clock_icon, 0, 0, 1, 1, Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignBottom)
+        blue_box_layout.addWidget(cycle_play_button, 0, 0, 1, 1, Qt.AlignmentFlag.AlignCenter)
+        main_layout.addWidget(blue_box, 0, 0, 1, 2)
         self.setLayout(main_layout)
 
         #connect buttons
