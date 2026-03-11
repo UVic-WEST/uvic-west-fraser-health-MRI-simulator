33e1dc6130579ec2fcd903bccf652779675fe9d9
rachangrewal <rachankgrewal@gmail.com>
2026-03-10 18:17:16 -0700

    Setup Raspberry Pi baseline files for embedded layer (#75)

---
+++ embedded/__init__.py
@@ -0,0 +1,13 @@
+"""
+Embedded hardware interface layer for the MRI simulator.
+
+This package contains Raspberry Pi-side modules responsible for
+interacting with hardware components such as:
+
+- audio playback
+- lighting control
+- GPIO interfaces
+- serial communication with the STM32 controller
+
+Direct hardware testing is deferred until Raspberry Pi deployment.
+"""
