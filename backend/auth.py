e6a10be1beefe33b5246d91063118d95dae3d31c
natalyaa-01 <natalyaa01@icloud.com>
2026-03-11 18:38:15 -0700

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,17 +1,24 @@
+from dataclasses import dataclass
+
 class Auth:
     """
     Handles the authentication of the MRI simulator.
     """
-    # whether a user can attempt to login or not
-    can_attempt_login = True
-    is_authenticated = False
-    remaining_attempts = 3
-
-    def __init__(self, correct_pin):
+    def __post_init__(self, correct_pin):
         self._correct_pin = correct_pin
+        
+        can_attempt_login = True  # whether a user can attempt to login or not
+        is_authenticated = False # whether PIN entered is the correct PIN
+        remaining_attempts = 3  # sign in attempts remaining before 30 second timeout
 
+        if len(correct_pin) != 4:
+            raise ValueError(f"PIN must be four digits long")
+
+    
     def login(self, pin):
+        
         return
+
     
     def logout(self):
         return
