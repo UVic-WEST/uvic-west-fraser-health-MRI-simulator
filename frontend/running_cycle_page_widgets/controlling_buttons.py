35764e1dba4bed81916f79eae224b70e664f3ae7
Purnima <pverma@uvic.ca>
2026-03-22 15:24:56 -0700

    Changed the name of the .png file for the resume button in the controlling_buttons.py file to match the new name of the .png file.

--- frontend/running_cycle_page_widgets/controlling_buttons.py
+++ frontend/running_cycle_page_widgets/controlling_buttons.py
@@ -21,7 +21,7 @@ class ControllingButtons(QWidget):
         # # **IMPORTANT:** set layout to this widget
         self.pause_button_asset_path = 'resources/cycle_running_page_assets/pause_button.png'
         self.stop_button_asset_path = 'resources/cycle_running_page_assets/stop_button.png'
-        self.resume_button_asset_path = 'resources/cycle_running_page_assets/resume_button'
+        self.resume_button_asset_path = 'resources/cycle_running_page_assets/resume_button.png'
         
         #setting up widgets
         #pause button
