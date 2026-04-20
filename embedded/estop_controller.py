55f877aa1b03f2ee54fb324fb13121b9b25eb090
megan <megand0905@gmail.com>
2026-04-19 20:43:49 -0700

    Add embedded e-stop

--- embedded/estop_controller.py
+++ embedded/estop_controller.py
@@ -3,6 +3,7 @@ from PySide6.QtCore import (
 )
 from PySide6.QtWidgets import QWidget
 from embedded.temp_estop import TempEstop
+import platform
 
 class EStopController(QWidget):
 
@@ -10,18 +11,24 @@ class EStopController(QWidget):
 
     def __init__(self, parent=None):
         """
-        When estop is hit it sends a signal to the frontend to close all functions
-        UPDATE THIS
+        Monitors the physical E-Stop button on GPIO21. Emits estop_active(True)
+        when pressed and estop_active(False) when released.
         """
         super().__init__(parent)
         self.temp_button = TempEstop(self)
+        
+        if platform.system() == "Linux": # Raspberry pi 
+            print("Running on Raspberry Pi")
+            try:
+                from gpiozero import Button
+                self.estop = Button(21)
+                self.estop.when_pressed = lambda: self.estop_pressed(True)
+                self.estop.when_released = lambda: self.estop_released(False)
+            except Exception as e:
+                print("Failed to configure GPIO. Error: ", e)
+
+        else: # Skip GPIO configuration
+            pass
 
     def estop_event(self, active):
         self.estop_active.emit(active)
-        
-
-
-
-
-
-
